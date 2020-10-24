b55513313843c39af2de249984a23b27e08b4116
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-10-25 08:19:34 +0900

    デバイスのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :installs
+  devise_for :users
   get 'messages/index'
   get 'message/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
