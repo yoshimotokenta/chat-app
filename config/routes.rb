2268f195b73115c6c27147457d1a378c83053973
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-10-25 10:58:48 +0900

    userテーブルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :installs
   devise_for :users
+  devise_for :installs
   get 'messages/index'
   get 'message/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
