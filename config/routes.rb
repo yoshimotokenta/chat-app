dfab05b11270e258d08e043b9bdfe3e6f25d1980
yoshimotokenta <hiraidai10@yahoo.co.jp>
2020-10-25 14:59:54 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :installs
-  get 'messages/index'
-  get 'message/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
